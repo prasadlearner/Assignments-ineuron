3c779c572fcc4680bb8dc67b9c17d4738f9d958a
prasadlearner <prasadchikkam1999@gmail.com>
2023-06-26 22:30:22 +0530

    FirstCommit

---
+++ Quiz3_Question_1/src/Stack.java
@@ -0,0 +1,37 @@
+
+/*
+ * 
+ * Implement a stack using an array in Java. Include the necessary methods such as push, pop, and isEmpty.
+ */
+
+class Stack {
+	int maxSize;
+	int top;
+	int[] array;
+
+	public Stack(int capacity) {
+		maxSize = capacity;
+		top = -1;
+		array = new int[maxSize];
+	}
+
+	public void push(int value) {
+		if (top == maxSize - 1) {
+			System.out.println("Stack overflow");
+			return;
+		}
+		array[++top] = value;
+	}
+
+	public int pop() {
+		if (top == -1) {
+			System.out.println("Stack underflow");
+			return -1;
+		}
+		return array[top--];
+	}
+
+	public boolean isEmpty() {
+		return (top == -1);
+	}
+}
